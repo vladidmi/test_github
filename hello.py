5a2ccef29c163e47e6653477c2725a2a32fc5fa8
vladidmi <86980077+vladidmi@users.noreply.github.com>
2022-09-10 22:28:24 +0200

    added class functions

--- hello.py
+++ hello.py
@@ -7,12 +7,21 @@ print('jeremy')
 
 
 class Person:
-    def __init__(self,name):
+    def __init__(self,name, age=None):
         self.name = name
+        self.age = age
 
     def greet(self):
         print(f'hello {self.name}')
 
+    def show_age(self):
+        print(f'My age is {self.age}')
+
 Oliver = Person ('Oliver')
 
 Oliver.greet()
+
+Ham = Person ('Ham',40)
+
+Ham.greet()
+Ham.show_age()
