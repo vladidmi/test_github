05946ecdf7ce80c08a2eaff32b7f68890c0c827f
vladidmi <86980077+vladidmi@users.noreply.github.com>
2022-09-10 22:31:29 +0200

    add job

--- hello.py
+++ hello.py
@@ -7,12 +7,17 @@ print('jeremy')
 
 
 class Person:
-    def __init__(self,name):
+    def __init__(self,name, profession = None):
         self.name = name
+        self.profession = profession
 
     def greet(self):
         print(f'hello {self.name}')
 
-Oliver = Person ('Oliver')
+    def show_job(self):
+        print(f'I\'m a cool {self.profession}')
+
+Oliver = Person ('Oliver', 'worker')
 
 Oliver.greet()
+Oliver.show_job()
